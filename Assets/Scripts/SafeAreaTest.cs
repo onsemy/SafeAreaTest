daeae336440a34f053d62966e0689b0f5c8a5408
Jeongjin Oh <onsemy@gmail.com>
2020-01-28 16:37:17 +0900

    테스트했던 파일 일단 추가

---
+++ Assets/Scripts/SafeAreaTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeAreaTest : MonoBehaviour
+{
+    private Rect _lastSafeArea;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        if (_lastSafeArea != Screen.safeArea)
+        {
+            _lastSafeArea = Screen.safeArea;
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (_lastSafeArea != Screen.safeArea)
+        {
+            _lastSafeArea = Screen.safeArea;
+        }
+
+        GUI.Box(_lastSafeArea, "Safe Area");
+    }
+
+    private void OnRectTransformDimensionsChange()
+    {
+        Debug.Log("[SafeAreaTest::OnRectTransformDimensionsChange] Event On!");
+        var currentOrientation = Screen.orientation;
+        if (_lastSafeArea != Screen.safeArea)
+        {
+            _lastSafeArea = Screen.safeArea;
+            Debug.Log("[SafeAreaTest::OnRectTransformDimensionsChange] Changed Safe Area!");
+        }
+    }
+}
